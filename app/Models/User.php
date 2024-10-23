e3dbab9b408f5ce848f6966af018d2e81574aab8
StivenChamorro <stivenchamorro493@gmail.com>
2024-10-23 10:23:18 -0500

    scopes de childrens añadidos

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -45,6 +47,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected $allowIncluded = ['Children']; //las posibles Querys que se pueden realizar
+    protected $allowFilter = ['id','name','lastname','age','nickname','relation','avatar','gender','user_id'];
+
     //relaciones a nivel de modelos
 
     // Un usuario puede tener muchos niños
@@ -55,5 +60,81 @@ class User extends Authenticatable
     {
         return $this->hasMany(Children::class); //hasMany se usa para obtener a todas las relaciones de uno a muchos
     }
-  
+
+    public function scopeIncluded(Builder $query)
+    {
+
+        if(empty($this->allowIncluded)||empty(request('included'))){// validamos que la lista blanca y la variable included enviada a travez de HTTP no este en vacia.
+            return;
+        }
+
+
+        $relations = explode(',', request('included')); //['posts','relation2']//recuperamos el valor de la variable included y separa sus valores por una coma
+
+       // return $relations;
+
+        $allowIncluded = collect($this->allowIncluded); //colocamos en una colecion lo que tiene $allowIncluded en este caso = ['posts','posts.user']
+
+        foreach ($relations as $key => $relationship) { //recorremos el array de relaciones
+
+            if (!$allowIncluded->contains($relationship)) {
+                unset($relations[$key]);
+            }
+        }
+        $query->with($relations); //se ejecuta el query con lo que tiene $relations en ultimas es el valor en la url de included
+
+        //http://api.codersfree1.test/v1/categories?included=posts
+
+
+    }
+    //return $relations;
+    // return $this->allowIncluded;
+    public function scopeFilter(Builder $query)
+    {
+
+        if (empty($this->allowFilter) || empty(request('filter'))) {
+            return;
+        }
+
+        $filters = request('filter');
+        $allowFilter = collect($this->allowFilter);
+
+        foreach ($filters as $filter => $value) {
+
+            if ($allowFilter->contains($filter)) {
+
+                $query->where($filter, 'LIKE', '%' . $value . '%');
+            }
+        }
+
+        //http://api.codersfree1.test/v1/categories?filter[name]=depo
+        //http://api.codersfree1.test/v1/categories?filter[name]=posts&filter[id]=2
+
+    }
+
+    public function scopeSort(Builder $query)
+    {
+
+        if (empty($this->allowSort) || empty(request('sort'))) {
+            return;
+        }
+
+        $sortFields = explode(',', request('sort'));
+        $allowSort = collect($this->allowSort);
+
+        foreach ($sortFields as $sortField) {
+
+            $direction = 'asc';
+
+            if (substr($sortField, 0, 1) == '-') {
+                $direction = 'desc';
+                $sortField = substr($sortField, 1);
+
+            if ($allowSort->contains($sortField)) {
+                $query->orderBy($sortField, $direction);
+            }
+        }
+        //http://api.codersfree1.test/v1/categories?sort=name
+        }
+    }
 }
