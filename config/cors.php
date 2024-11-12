12fcd22874ac89407dfda9feca785e959bc84588
StivenChamorro <stivenchamorro493@gmail.com>
2024-11-11 19:57:05 -0500

    cors nuevamente modificado

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,22 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['v1/*'], // Aplica CORS a todas las rutas bajo el prefijo 'v1'
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // Permite todos los métodos HTTP
 
-    'allowed_origins' => ['http://localhost:8000', 'http://127.0.0.1:8000'],
+    'allowed_origins' => [
+        'http://localhost:8000', 
+        'http://127.0.0.1:8000'
+    ], // Permite solo estos orígenes para solicitudes de desarrollo local
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [], // Opcional: Usa patrones si necesitas coincidencias parciales
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // Permite todos los encabezados
 
-    'exposed_headers' => [],
+    'exposed_headers' => [], // Puedes especificar los encabezados que deseas exponer al cliente
 
-    'max_age' => 0,
+    'max_age' => 0, // Controla el tiempo de vida de la política en caché
 
-    'supports_credentials' => false,
-
+    'supports_credentials' => false, // Generalmente false cuando usas JWT
 ];
