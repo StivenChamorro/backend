9962fcb457be30952f83c25d90092b768cd930e5
StivenChamorro <stivenchamorro493@gmail.com>
2024-12-02 23:04:47 -0500

    correciones al database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,6 +25,10 @@ class DatabaseSeeder extends Seeder
             'password' => '123456789',
             'pin' => '1234'
         ]);*/
+
+          // Vaciar las tablas antes de insertar datos
+        DB::table('answers')->truncate();
+        DB::table('questions')->truncate();
 
         $this->call([
             QuestionSeeder::class,
