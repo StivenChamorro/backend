c848fc60aa3ef40efbe43ddcab72ed1113a1ddbb
StivenChamorro <stivenchamorro493@gmail.com>
2024-12-03 00:04:27 -0500

    s

--- database/migrations/2024_10_23_161708_create_topics_table.php
+++ database/migrations/2024_10_23_161708_create_topics_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image'); //url de la imagen del usuario
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
         });
     }
